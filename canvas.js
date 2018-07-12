88025a43c1d94ef254757e199d3ad2a1f981e064
David <sunshinesan00@gmail.com>
2018-07-12 20:29:15 +0100

    Change skip functionality and change button handling to be dynamic
    Skip button now can only be used (before it couldn't be) while the scene is playing. Makes the scene counter go to the confession scene count and guides the scene there.
    
    Added master button function that takes in an argument for the scenario number, assigned by the html side of the button. THIS MIGHT NEED TO CHANGE LATER?
    
    Need to stop music when skip is engaged.
    Need to check whether string is empty when clicking on the button that accepts user input.

--- canvas.js
+++ canvas.js
@@ -4,10 +4,11 @@ CLASS DEFINITIONS
 
 class Scenario //array of scenes + information about voice box positioning
 {
-	constructor(scenarioName, scenes)
+	constructor(scenarioName, scenes, proposal)
 	{
 		this.scenes = scenes;
 		this.scenarioName = scenarioName;
+		this.proposal = proposal; //scene # for proposal
 	}
 
 	toString()
@@ -19,22 +20,22 @@ class Scenario //array of scenes + information about voice box positioning
 
 class Scene
 {
-	constructor(imgSource, duration, sounds, confession)
+	constructor(imgSource, duration, sounds)
 	{
 		this.duration = duration;
 		this.imgSource = imgSource;
 		this.sounds = sounds; //array of SceneSound objects
-		this.confession = confession; //boolean whether this scene is the proposal theme
+		//this.confession = confession; //boolean whether this scene is the proposal theme
 	}
 }
 
 class SceneSound
 {
-	constructor(songName, startTime, confession)
+	constructor(songName, startTime)
 	{
 		this.songName = songName;
 		this.startTime = startTime;
-		this.confession = confession;
+		//this.confession = confession;
 	}
 
 	playSong()
@@ -68,27 +69,51 @@ const DEFAULT_LINE = "The brown fox jumps over the lazy dog."
 GLOBAL VARIABLES
 **********************/
 
-let scene_i = 0;
+let scene_i = 0; //current scene #
+let currentScenario = 0; //current scenario #
+let skip = false; //flag, playScenario checks this at first, if true, exit out of recursion.
 
 //KITCHEN SCENE
 
-let scene1 = new Scenario
+let scenario_0 = new Scenario
 (
 	"Kitchen Proposal",
 	[
-		new Scene('1-1', 4000, [new SceneSound('intro', 2000, false)], false),
-		new Scene('1-2', 1200, null, false),
-		new Scene('1-2b', 1800, null, false),
-		new Scene('1-2c', 1900, null, false),
-		new Scene('1-2b', 1800, null, false),
-		new Scene('1-3', 2700, null, false),
-		new Scene('1-3b', 2000, null, false),
-		new Scene('1-4', 2000, null, false),
-		new Scene('1-4b', 5000, [new SceneSound('plove', 1000, true)], true),
-		new Scene('1-5', 5000, null, false)
-	]
+		new Scene('1-1.jpg', 4000, [new SceneSound('intro', 2000)]),
+		new Scene('1-2.jpg', 1200, null),
+		new Scene('1-2b.jpg', 1800, null),
+		new Scene('1-2c.jpg', 1900, null),
+		new Scene('1-2b.jpg', 1800, null),
+		new Scene('1-3.jpg', 2700, null),
+		new Scene('1-3b.jpg', 2000, null),
+		new Scene('1-4.jpg', 2000, null),
+		new Scene('1-4b.jpg', 5000, [new SceneSound('plove', 1000)]),
+		new Scene('1-5.jpg', 5000, null)
+	],
+	8
 ); //contains individual scene objects
 
+//TRAIN SCENE
+
+let scenario_1 = new Scenario
+(
+	"Train Proposal",
+	[
+		new Scene('1.png', 5000, [new SceneSound('intro', 0)]),
+		new Scene('2.png', 5000, null),
+		new Scene('3.png', 5000, null),
+		new Scene('4.png', 5000, null),
+		new Scene('5.png', 5000, [new SceneSound('will', 1000)])
+	],
+	3
+);
+
+//LIST OF SCENARIOS
+
+let scenarioList = 
+[
+	scenario_0, scenario_1
+];
 
 let scene_elem = document.getElementById('scene');
 let sceneWindow = document.getElementById('scene-window');
@@ -97,7 +122,9 @@ let line_text_elem = document.getElementById('line-text');
 var voiceBox = document.getElementById("voice-box");
 var supportMsg = document.getElementById("msg");
 var confessionButton = document.getElementById("confession-button");
-var playSceneButton = document.getElementById("play-button");
+var playScenarioButton = document.getElementById("play-button");
+var playScenarioButton2 = document.getElementById("play-button2");
+
 var skipButton = document.getElementById("skip-button");
 var speechMsgInput = document.getElementById("speech-msg");
 var voiceSelect = document.getElementById("voice");
@@ -111,41 +138,62 @@ var wordIndex = 0;
 /*********************
 EVENT LISTENERS
 **********************/
-
-if(confessionButton)
-{
-	confessionButton.addEventListener
-	('click', function(e)
-		{
-			if(speechMsgInput.value.length > 0)
-			{
-				sceneSlide(scene1, false);
-				
-			}
-		}
-	);
-}
-
-// if(playSceneButton)
+///////////////////////////////////////////////////////////////////////////
+//need the confession button check for length of input > 0 in the buttonclick master functionXXXXX
+/////////////////////////////////////////////////////////////////////////
+// if(confessionButton)
 // {
-playSceneButton.addEventListener
-(
-	'click', function(e)
-	{
-		//scene.style.display = 'flex';
-		console.log('scene button pressed. scene arg is ' + scene1);
-		sceneSlide(scene1, false);
-	}
-
-);
+// 	confessionButton.addEventListener
+// 	('click', function(e)
+// 		{
+// 			if(speechMsgInput.value.length > 0)
+// 			{
+// 				playScenario(currentScenario, false);
+// 			}
+// 		}
+// 	);
 // }
+
+// if(playScenarioButton)
+// {
+// playScenarioButton.addEventListener
+// (
+// 	'click', function(e)
+// 	{
+// 		playScenario(scenario_0, false);
+// 	}
+
+// );
+// //	}
+// playScenarioButton2.addEventListener
+// (
+// 	'click', function(e)
+// 	{
+// 		playScenario(scene2, false);
+// 	}
+
+// );
 
 skipButton.addEventListener
 ('click', function(e)
 	{
-		sceneSlide(scene1, true);
+		scene_i = scenarioList[currentScenario].proposal - 1;
+		skip = true;
+		playScenario(scenarioList[currentScenario]);
 	}
 );
+
+//master button listener
+function buttonClick(scenario) //scenario = x, then scenario = currentScenario
+{
+	if(scenario < 0)
+	{
+		scenario = currentScenario;
+	}
+
+	currentScenario = scenario;
+	playScenario(scenarioList[currentScenario]);
+}
 
 
 /*********************
@@ -189,38 +237,16 @@ function speak(text)
 	window.speechSynthesis.speak(msg);
 }
 
-function sceneSlide(scenario, skip) //plays the scene, if skip is true, goes to the proposal right away
+function playScenario(scenario) //plays the scene, if skip is true, goes to the proposal right away
 {
-	voiceBox.style.display = 'none';
-	//playSceneButton.style.display = 'none';
-	//let songWaitTime;
-
-	//handles the skip button
-	if(skip)
-	{
-		console.log("skip activated");
-		for(let skip_i = 0; skip_i < scenario.scenes.length; skip_i++)
-		{
-			console.log(skip_i);
-			if(scenario.scenes[skip_i].confession == true)
-			{
-				scene_i = skip_i
-				break;
-			}
-		}
-	}
-	else
-	{
-		console.log('no skip detected');
-	}
-
+	if(!skip)
+		voiceBox.style.display = 'none';
 	let songWaitTime = 0;
-	console.log('scene is ' + scenario);
-	console.log('scene duration is ' + scenario.scenes[scene_i].imgSource)
+	console.log('XXXscene is ' + scene_i);
 	let waitTime = scenario.scenes[scene_i].duration;
 
-	scene_elem.style.backgroundImage = `url(${scenario.scenes[scene_i].imgSource}.jpg)`;
-	if(scenario.scenes[scene_i].confession) //handles the scene where user inputs the dialogue
+	scene_elem.style.backgroundImage = `url(${scenario.scenes[scene_i].imgSource})`;
+	if(scene_i == scenario.proposal) //handles the scene where user inputs the dialogue
 	{
 		console.log(speechMsgInput.value);
 		if(speechMsgInput.value == '')
@@ -239,7 +265,7 @@ function sceneSlide(scenario, skip) //plays the scene, if skip is true, goes to 
 				scene_i++;
 				sceneEnd(scenario, waitTime);
   			}
-
+  			console.log('proposal number is ' + scene_i);
 			speak(speechMsgInput.value);		
 		}
 		
@@ -269,10 +295,15 @@ function soundIterator(scenario)
 
 function sceneEnd(scenario, waitTime)
 {
+	if(skip)
+	{
+		skip = false;
+		return;
+	}
 	if(scene_i < scenario.scenes.length)
 		{
-			console.log('wait time: ' + waitTime);
-			setTimeout(function(){sceneSlide(scenario)}, waitTime);
+			
+			setTimeout(function(){playScenario(scenario)}, waitTime);
 		}
 		else
 		{
@@ -281,10 +312,17 @@ function sceneEnd(scenario, waitTime)
 }
 
 function audioFunction(songName)
-	{
-		var audio = new Audio(`${songName}.ogg`);
-		audio.play();
-	}
+{
+	var audio = new Audio(`${songName}.ogg`);
+	audio.play();
+
+	// skipButton.addEventListener
+	// ('click', function(e)
+	// 	{
+	// 		audio.pause();
+	// 	}
+	// );
+}
 
 if('speechSynthesis' in window)
 {
