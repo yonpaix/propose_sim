b92b4e4c7ec339f4866a21e9feef1084f31ca845
David <sunshinesan00@gmail.com>
2018-07-14 23:25:46 +0100

    Add moment sound effect
    Added moment Sound Effect during proposal scene before input. Stand alone, doesn't need to be queued.
    
    Added Office Scene

--- canvas.js
+++ canvas.js
@@ -88,14 +88,14 @@ let scenario_0 = new Scenario
 (
 	"Kitchen Proposal",
 	[
-		new Scene('1-1.jpg', 4000, [new SceneSound('intro', 0)]),
-		new Scene('1-2.jpg', 1200, null),
-		new Scene('1-2b.jpg', 1800, null),
-		new Scene('1-2c.jpg', 1900, null),
-		new Scene('1-2b.jpg', 1800, null),
-		new Scene('1-3.jpg', 2700, null),
-		new Scene('1-3b.jpg', 2000, null),
-		new Scene('1-4.jpg', 2000, null),
+		new Scene('1-1.jpg', 3400, [new SceneSound('sounds/kitchen-love', 0)]),
+		new Scene('1-2.jpg', 1800, null),
+		new Scene('1-2b.jpg', 2500, null),
+		new Scene('1-2c.jpg', 4100, null),
+		new Scene('1-2b.jpg', 2600, null),
+		new Scene('1-3.jpg', 1700, null),
+		new Scene('1-3b.jpg', 1600, null),
+		new Scene('1-4.jpg', 3000, null),
 		new Scene('1-4b.jpg', 5000, [new SceneSound('plove', 1000)]),
 		new Scene('1-5.jpg', 5000, null)
 	],
@@ -117,11 +117,28 @@ let scenario_1 = new Scenario
 	3
 );
 
+let scenario_2 = new Scenario
+(
+	"Office Proposal",
+	[
+		new Scene('images/2-1.jpg', 3000, [new SceneSound('sounds/office-love', 0)]),
+		new Scene('images/2-2.jpg', 3000, null),
+		new Scene('images/2-3.jpg', 2800, null),
+		new Scene('images/2-4.jpg', 1100, null),
+		new Scene('images/2-5.jpg', 2400, null),
+		new Scene('images/2-6.jpg', 1900, null),
+		new Scene('images/2-7.jpg', 2500, null),
+		new Scene('images/2-8.jpg', 2800, null),
+		new Scene('images/2-9.jpg', 5000, [new SceneSound('plove', 1000)]),
+		new Scene('images/2-10.jpg', 5000, null)
+	],
+	8
+);
 //LIST OF SCENARIOS
 
 let scenarioList = 
 [
-	scenario_0, scenario_1
+	scenario_0, scenario_1, scenario_2
 ];
 
 let soundList = []; //list of sounds that can be accessed globally. Needed to turn them off whenever a scene is skipped, and to keep tabs.
@@ -294,7 +311,16 @@ function playScenario(scenario) //plays the scene, if skip is true, goes to the 
 	{
 		if(speechMsgInput.value == '')
 		{
-			voiceBox.style.display = 'initial';
+			setTimeout
+			(
+				function()
+				{
+					var moment = new Audio('sounds/moment.mp3');
+					moment.play();
+					voiceBox.style.display = 'initial';
+				},
+				2000
+			);
 			
 			scene_i = 0;
 			console.log('checking out');
@@ -322,7 +348,7 @@ function playScenario(scenario) //plays the scene, if skip is true, goes to the 
 				{
 					speakTxt(speechMsgInput.value);
 				},
-				1000
+				1500
 			);
 		}
 		
